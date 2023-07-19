3b3d9745979aca155b0fc9242da38949460df195
Lisandro Crespo <hey@lisandroct.com>
2023-07-19 14:12:10 -0500

    Add Div

---
+++ Runtime/Content/Div.cs
@@ -0,0 +1,23 @@
+using RishUI;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Roots
+{
+    public partial class Div : VisualElement, IVisualElement
+    {
+        VisualElement IElement.GetDOMChild() => this;
+        
+        private PickingManager PickingManager { get; }
+        PickingManager ICustomPicking.Manager => PickingManager;
+
+        public Div()
+        {
+            PickingManager = new DefaultPickingManager(this);
+        }
+
+        void IVisualElement.Setup() { }
+
+        public override bool ContainsPoint(Vector2 localPoint) => PickingManager.ContainsPoint(localPoint);
+    }
+}

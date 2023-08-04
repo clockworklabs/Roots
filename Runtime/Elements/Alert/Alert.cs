524caad28fbbe1973d04fc18f4638c4a1ade18c7
Lisandro Crespo <hey@lisandroct.com>
2023-08-04 14:26:18 -0500

    Add utilities to Alert

--- Runtime/Elements/Alert/Alert.cs
+++ Runtime/Elements/Alert/Alert.cs
@@ -6,25 +6,37 @@ namespace Roots
     {
         public enum Context { Primary, Secondary, Success, Danger, Warning, Info, Light, Dark }
         
-        protected override Element Render() => Div.Create(className:  ("alert", Props.context switch
+        protected override Element Render()
         {
-            Context.Primary => "alert-primary",
-            Context.Secondary => "alert-secondary",
-            Context.Success => "alert-success",
-            Context.Danger => "alert-danger",
-            Context.Warning => "alert-warning",
-            Context.Info => "alert-info",
-            Context.Light => "alert-light",
-            Context.Dark => "alert-dark",
-            _ => string.Empty
-        }), utilities: Props.utilities, children: Props.children);
+            var descriptor = Props.descriptor;
+            descriptor.className = new ClassName(descriptor.className)
+            {
+                "alert",
+                Props.context switch
+                {
+                    Context.Primary => "alert-primary",
+                    Context.Secondary => "alert-secondary",
+                    Context.Success => "alert-success",
+                    Context.Danger => "alert-danger",
+                    Context.Warning => "alert-warning",
+                    Context.Info => "alert-info",
+                    Context.Light => "alert-light",
+                    Context.Dark => "alert-dark",
+                    _ => string.Empty
+                }
+            };
+            
+            return Div.Create(descriptor, utilities: Props.utilities, children: Props.children);
+        }
     }
 
     [RishValueType]
     public struct AlertProps
     {
-        public Alert.Context context;
+        [DOMDescriptor]
+        public DOMDescriptor descriptor;
         public Utilities utilities;
+        public Alert.Context context;
         public Children children;
     }
 }

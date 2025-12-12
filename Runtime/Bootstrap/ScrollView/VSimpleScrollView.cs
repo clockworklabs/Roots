35a44eacc69d8dab04f53bf5251e759a33087efb
Lisandro Crespo <lisandroct@gmail.com>
2025-12-12 07:53:14 -0600

    Disable SimpleScrollView

--- Runtime/Bootstrap/ScrollView/VSimpleScrollView.cs
+++ Runtime/Bootstrap/ScrollView/VSimpleScrollView.cs
@@ -1,35 +1,35 @@
-using RishUI;
-
-namespace Roots.Bootstrap
-{
-    public partial class VSimpleScrollView : RishElement<VSimpleScrollViewProps>
-    {
-        protected override Element Render() => SimpleScrollView.Create(
-            name: Props.descriptor.name,
-            className: Props.descriptor.className,
-            style: Props.descriptor.style,
-            direction: SimpleScrollView.Direction.Vertical,
-            inverted: Props.inverted,
-            gap: Props.gap,
-            mouseWheelSensitivity: Props.mouseWheelSensitivity,
-            elementsSize: Props.elementsSize,
-            alwaysMountedIndices: Props.alwaysMountedIndices,
-            children: Props.children);
-    }
-
-    [RishValueType]
-    public struct VSimpleScrollViewProps
-    {
-        [DOMDescriptor]
-        public DOMDescriptor descriptor;
-        
-        public bool inverted;
-        public int gap;
-        
-        public float mouseWheelSensitivity;
-        public float? elementsSize;
-        
-        public RishList<int> alwaysMountedIndices;
-        public Children children;
-    }
-}
+// using RishUI;
+//
+// namespace Roots.Bootstrap
+// {
+//     public partial class VSimpleScrollView : RishElement<VSimpleScrollViewProps>
+//     {
+//         protected override Element Render() => SimpleScrollView.Create(
+//             name: Props.descriptor.name,
+//             className: Props.descriptor.className,
+//             style: Props.descriptor.style,
+//             direction: SimpleScrollView.Direction.Vertical,
+//             inverted: Props.inverted,
+//             gap: Props.gap,
+//             mouseWheelSensitivity: Props.mouseWheelSensitivity,
+//             elementsSize: Props.elementsSize,
+//             alwaysMountedIndices: Props.alwaysMountedIndices,
+//             children: Props.children);
+//     }
+//
+//     [RishValueType]
+//     public struct VSimpleScrollViewProps
+//     {
+//         [DOMDescriptor]
+//         public DOMDescriptor descriptor;
+//         
+//         public bool inverted;
+//         public int gap;
+//         
+//         public float mouseWheelSensitivity;
+//         public float? elementsSize;
+//         
+//         public RishList<int> alwaysMountedIndices;
+//         public Children children;
+//     }
+// }
